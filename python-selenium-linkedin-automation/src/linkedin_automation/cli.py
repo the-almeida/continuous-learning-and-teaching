9e6b9cfe9b31e9394ce3cb18401a6fe5f4753a0d
Gustavo Almeida <gspalmeida@icloud.com>
2025-11-03 12:56:53 -0300

    learning: py loops, dependencies and adding a funny tone to the hello world

--- python-selenium-linkedin-automation/src/linkedin_automation/cli.py
+++ python-selenium-linkedin-automation/src/linkedin_automation/cli.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from time import localtime
+
 import os
 import sys
 from typing import Any, Optional
@@ -35,16 +37,59 @@ if TyperApp is not None:
         """Print active configuration and environment sanity."""
         settings = _build_settings()
         logger.info("browser=%s headless=%s", settings.browser, settings.headless)
-        logger.info("timeouts: page_load=%ss slowmo=%sms", settings.page_load_timeout_sec, settings.slowmo_ms)
+        logger.info(
+            "timeouts: page_load=%ss slowmo=%sms",
+            settings.page_load_timeout_sec,
+            settings.slowmo_ms,
+        )
         print("CLI info: OK")
+
     @app_typer.command()
     def hello() -> None:
+        activities = {
+            8: "Sleeping",
+            9: "Commuting",
+            17: "Working",
+            18: "Commuting",
+            20: "Eating",
+            22: "Resting",
+        }
+        time_now = localtime()
+        hour = time_now.tm_hour
+        for activity_time in sorted(activities.keys()):
+            if hour < activity_time:
+                print(
+                    "Judging by the current time you should be: \033[1m\033[36m{}\033[0m".format(
+                        activities[activity_time]
+                    )
+                )
+                break
+        else:
+            print("Unknown, AFK or sleeping!")
+
+        print("--------------------------------")
         """Print active configuration and environment sanity in a friendly way."""
         settings = _build_settings()
         logger.info("browser=%s headless=%s", settings.browser, settings.headless)
-        logger.info("timeouts: page_load=%ss slowmo=%sms", settings.page_load_timeout_sec, settings.slowmo_ms)
-        name = input("What's your name? ")
-        print(f"Hi {name}, the information above looks good? If yes, let's get this project started!")
+        logger.info(
+            "timeouts: page_load=%ss slowmo=%sms",
+            settings.page_load_timeout_sec,
+            settings.slowmo_ms,
+        )
+        print("Hi Gus, the information above looks good? If yes, let's get this project started!")
+        print("\n\n--------------------------------")
+        print("Let's sing a song about bottles of beer!")
+        print("Below some experimentational code to play with Python loops and formatting.")
+        REFRAIN = """
+            {} bottles of beer on the wall,
+            {} bottles of beer,
+            take one down, pass it around,
+            {} bottles of beer on the wall!
+        """
+        bottles_of_beer = 9
+        while bottles_of_beer > 1:
+            print(REFRAIN.format(bottles_of_beer, bottles_of_beer, bottles_of_beer - 1))
+            bottles_of_beer -= 1
 
 else:
 
@@ -57,17 +102,22 @@ else:
             settings = _build_settings()
             logger.info("browser=%s headless=%s", settings.browser, settings.headless)
             logger.info(
-                "timeouts: page_load=%ss slowmo=%sms", settings.page_load_timeout_sec, settings.slowmo_ms
+                "timeouts: page_load=%ss slowmo=%sms",
+                settings.page_load_timeout_sec,
+                settings.slowmo_ms,
             )
             print("CLI info: OK")
         if args.hello is not None:
             settings = _build_settings()
             logger.info("browser=%s headless=%s", settings.browser, settings.headless)
             logger.info(
-                "timeouts: page_load=%ss slowmo=%sms", settings.page_load_timeout_sec, settings.slowmo_ms
+                "timeouts: page_load=%ss slowmo=%sms",
+                settings.page_load_timeout_sec,
+                settings.slowmo_ms,
             )
-            name = input("What's your name?")
-            print(f"Hi {name}, the information above looks good? If yes, let's get this project started!")
+            print(
+                "Hi Gus, the information above looks good? If yes, let's get this project started!"
+            )
         else:
             parser.print_help()
 
@@ -75,4 +125,3 @@ else:
 
 # Expose a single name without redefinition warnings
 app = app_typer if TyperApp is not None else _argparse_app
-
