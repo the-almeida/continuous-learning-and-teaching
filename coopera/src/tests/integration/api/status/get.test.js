e9261bf2b90b70f16f51787d1a1aee3750722ba4
Gustavo Almeida <gspalmeida@icloud.com>
2025-11-17 11:48:08 -0300

    fix: typo on test description

--- coopera/src/tests/integration/api/status/get.test.js
+++ coopera/src/tests/integration/api/status/get.test.js
@@ -1,4 +1,4 @@
-test('GET to /api/status should returno 200', async () => { 
+test('GET to /api/status should return 200', async () => { 
     const response = await fetch("http://localhost:3000/api/status") 
     expect(response.status).toBe(200)
 })
