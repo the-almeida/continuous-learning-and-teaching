6755581e2a6304fd27f66591bcd6d55b8d441f88
Gustavo Almeida <gspalmeida@icloud.com>
2025-12-23 18:30:54 -0300

    chore: improved homepage copy

--- superdoc-nextjs-playground/app/page.tsx
+++ superdoc-nextjs-playground/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between p-16 bg-white dark:bg-black sm:items-start">
         <h1 className="font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Hi, I'm Gus, and I'm trying out the SuperDoc + NextJS. If it works you
-          should see my CV bellow 😊👇
+          Hi, I'm Gus, and I'm trying out the SuperDoc + NextJS. You should see
+          my CV bellow 😊👇
         </h1>
         <DocumentEditor document="/cvGustavoAlmeida.docx" />
       </main>
