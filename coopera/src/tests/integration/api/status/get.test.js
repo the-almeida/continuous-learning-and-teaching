5aba48f47db5c3da012037c5db30fa0cbf53bb8b
Gustavo Almeida <gspalmeida@icloud.com>
2026-01-22 16:42:48 -0300

    refactor(test): made fetch method explicit in status test (GET)

--- coopera/src/tests/integration/api/status/get.test.js
+++ coopera/src/tests/integration/api/status/get.test.js
@@ -1,4 +1,6 @@
 test('GET to /api/status should return 200', async () => { 
-    const response = await fetch("http://localhost:3000/api/status") 
+    const response = await fetch("http://localhost:3000/api/status", {
+        method: 'GET'
+    }) 
     expect(response.status).toBe(200)
 })
