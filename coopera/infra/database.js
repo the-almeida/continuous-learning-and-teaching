d0d16a40aceee39508c729c222e040b9317a561c
Gustavo Almeida <gspalmeida@icloud.com>
2026-02-19 13:20:42 -0300

    feat(database.js): add database pooling and make adapter robust to error handling
    Set up POSTGRES_POOL_MAX_CONNECTIONS env variable and migrated from pg Client to pg Pool

--- coopera/infra/database.js
+++ coopera/infra/database.js
@@ -1,20 +1,16 @@
-import { Client } from "pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  max: Number(process.env.POSTGRES_POOL_MAX_CONNECTIONS) || 10,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+});
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-
-  await client.connect();
-
-  const result = await client.query(queryObject);
-
-  await client.end();
-  return result;
+  return pool.query(queryObject);
 }
 
 const database = {
