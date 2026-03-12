2774e2bcff36a0ae6539a307d89c89744cd554f0
Gustavo Almeida <gspalmeida@icloud.com>
2026-03-12 11:24:40 -0300

    test: revert lint test file

--- coopera/src/app/api/status/route.ts
+++ coopera/src/app/api/status/route.ts
@@ -1,7 +1,6 @@
 import database from "@/infra/database";
 
 export async function GET() {
-  console.log("test lint error");
   const updatedAt = new Date().toISOString();
 
   const databaseName = process.env.POSTGRES_DB;
